c1b0a8d698361934be3c8a3bf9ef05444fccdfd1
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-15 01:41:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
-  attr_accessor :total
+  attr_accessor :total :discount
   def initialize(employee_discount = 0)
     @total = 0
-    
+    @discount = employee_discount
   end
 end #end of class 
