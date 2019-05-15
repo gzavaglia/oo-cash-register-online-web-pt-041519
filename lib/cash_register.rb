307ade67ee84bef79b36ef87c49e9fba3c10bd11
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-15 02:02:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,12 +12,13 @@ class CashRegister
   end
   
   def apply_discount
-    #if @discount != 0
+    if @discount != 0
       pc_discount = 1 - @discount/100.0 
       @total = @total*pc_discount    
       return "After the discount, the total comes to $#{@total.to_i}."
+    else
       
       #binding.pry
-    #end 
+    end 
   end
 end #end of class 
