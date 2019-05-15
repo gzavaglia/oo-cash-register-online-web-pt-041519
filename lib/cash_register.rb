bfe53d589d4ba9510ea396e777154248aaa61eff
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-15 02:06:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     new_total = price*quantity
     @total += new_total
     @item = itemx
-    array_of_items << itemx
+    #array_of_items << itemx
   end
   
   def apply_discount
